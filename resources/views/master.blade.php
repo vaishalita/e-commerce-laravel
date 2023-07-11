b14b44e848b12ff378772ce9c813076dbed6b715
vaishalita <vaishalitakate16@gmail.com>
2023-07-11 14:30:13 +0530

    Login page done

---
+++ resources/views/master.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Ecommerce Project</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+    <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+  </head>
+  <bocdy>
+    {{ View::make('header') }}
+    @yield('content')
+    <style>
+      .custom-login{
+        height: 500px;
+        padding-top: 100px;
+        padding-left: 350px;
+      }
+
+    </style>
+
+    {{ View::make('footer')}}
+  </body>
+</html>
